a753831c0c2ef0f6eeeccc4ff7314b1237c18f4c
Evgeniy Sharychenkov <sanctio@yandex.ru>
2023-07-23 16:40:43 +0300

    Initial commit

---
+++ src/main/java/ru/sanctio/progectlibraryboot/ProgectLibraryBootApplication.java
@@ -0,0 +1,14 @@
+package ru.sanctio.progectlibraryboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgectLibraryBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProgectLibraryBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sanctio/progectlibraryboot/ProgectLibraryBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sanctio.progectlibraryboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgectLibraryBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
